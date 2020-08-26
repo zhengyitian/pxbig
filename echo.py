916a5da4ef6061294b295e617fc858ee3d692eef
zhengyitian <37394193+zhengyitian@users.noreply.github.com>
2020-08-26 23:20:51 +0800

    Update echo.py

--- echo.py
+++ echo.py
@@ -5,19 +5,102 @@ from PyQt5.QtGui  import  *
 from PyQt5.QtWidgets  import  *
 from PyQt5.QtCore  import  *
 from PyQt5.QtNetwork import * 
+import socket
 
-xLen = 2244
-yLen = 2244
-xS = 800
-yS = 800
-yL = 50        
-xL = 50  
-drawP1 = 100
-drawP2 = 100
+xCapLen = 2244
+yCapLen = 2244
+drawP1 = 0
+drawP2 = 50
 maxX = 1366
 maxY = 768
 
+import threading
+g_lock = threading.Lock()
+g_para = (1000,100,100,100)
+g_buf = (b'0'*40000,100,100,0)
 
+def tcpGetBuf(a,b,c,d):
+    s = socket.socket()
+    add = ('10.23.185.230',8899)
+    s.connect(add)
+    s.sendall(struct.pack('i',a)+struct.pack('i',b)+struct.pack('i',c)+struct.pack('i',d))
+    d = b''
+    while True:
+        ss = s.recv(65536)
+        if not ss:
+            break
+        d+=ss
+    return d
+
+def thr():
+    global g_buf
+    co = 0
+    while True:
+        co =+ 1
+        g_lock.acquire()
+        a,b,c,d = g_para
+        g_lock.release()
+        try:
+            ss = tcpGetBuf(a,b,c,d)
+            if len(ss) != c*d*4:
+                continue
+        except:
+            continue
+        
+        g_lock.acquire()
+        g_buf = (ss,c,d,co)
+        g_lock.release()
+        
+class qThr(QThread):
+    def __init__(self):
+        super().__init__()
+        self.co = 0
+        
+    def run(self):
+        while True:
+            self.w()
+            
+    def w(self):
+        self.co += 1
+        s = QTcpSocket(self)
+        #s.connectToHost('10.23.185.230',8899)
+        s.connectToHost('127.0.0.1',8899)
+        r = s.waitForConnected(1000)
+        if not r:
+            s.deleteLater()
+            return
+        
+        g_lock.acquire()
+        a,b,c,d = g_para
+        g_lock.release()
+        s.write(struct.pack('i',a)+struct.pack('i',b)+struct.pack('i',c)+struct.pack('i',d))
+        r = s.waitForBytesWritten(1000)
+        if not r:
+            s.deleteLater()
+            return
+        
+        da = b''     
+        t = time.monotonic()
+        while True:
+            s.waitForReadyRead(1000)
+            if time.monotonic()-t>5:
+                s.deleteLater()
+                return
+            if not r:
+                continue
+            ss = s.readAll()
+            if not ss:
+                break
+            da += ss
+        s.deleteLater()    
+        if len(da) != c*d*4:
+            return
+        global g_buf
+        g_lock.acquire()
+        g_buf = (da,c,d,self.co)
+        g_lock.release()        
+
+            
 class myLine(QLineEdit):
     def __init__(self,text=''):
         super().__init__(text)
@@ -30,99 +113,23 @@ class myLine(QLineEdit):
         window.onCli(e)
 
 class ww(QWidget):
-    def __init__(self, arg0=None, arg1=None, arg2=None):
+    def __init__(self):
         super().__init__()
         t = QTimer(self)
-        t.timeout.connect(self.aa)
+        t.timeout.connect(self.onTimer)
         t.start(100)
-        self.data = b''
-        self.pic = None
-        self.tcpCli = None
-        self.ini()
-        self.posCache = None
+
         QCoreApplication.instance().installEventFilter(self)
         self.pause = False
         self.reso = 0
-        self.tcpStartTime = time.monotonic()
-        self.setGeometry(QRect(200,200,800,500))
-        
-    def drawOne(self,ty,v,i,j,im):
-        redCount = v/255*self.reso*self.reso/3
-        redCo = 0
-        if ty=='r':
-            cc = Qt.red
-            gap = 0
-        if ty=='g':
-            cc = Qt.green
-            gap = self.reso//3
-        if ty=='b':
-            cc = Qt.blue
-            gap = self.reso*2//3
-            
-        for ii in range(i*self.reso+gap,gap+i*self.reso+self.reso//3):
-            for jj in range(j*self.reso,(j+1)*self.reso):
-                if redCo >= redCount:
-                    im.setPixelColor(ii,jj,Qt.black)
-                    continue
-                
-                im.setPixelColor(ii,jj,cc)
-                redCo += 1  
-                
-    def draw(self):
-        if  not self.pic:
-            return        
-        painter = QPainter(self)
-        pic = self.pic
-        if self.reso == 0:
-            painter.drawImage(QRect(drawP1,drawP2,pic.width(),pic.height()),pic)    
-            return
-        pa = maxX
-        if pic.width()*self.reso < maxX:
-            pa = pic.width()*self.reso
-        pb = maxY
-        if pic.height()*self.reso < maxY:
-            pb = pic.height()*self.reso            
-        im = QPixmap(QSize(pa,pb)).toImage()
-        
-        for i in range(pic.width()):
-            for j in range(pic.height()):
-                if (i+1)*self.reso>maxX or (j+1)*self.reso>maxY:
-                    continue
-                c = pic.pixel(QPoint(i,j))
-                cc = QColor(c)
-                r,g,b = qRed(c),qGreen(c),qBlue(c)
-                self.drawOne('r',r,i,j,im)
-                self.drawOne('g',g,i,j,im)
-                self.drawOne('b',b,i,j,im)
-        painter.drawImage(QRect(drawP1,drawP2,im.width(),im.height()),im)    
-            
-    def onP(self,e):
-        self.pause = not self.pause
-        if self.pause:            
-            self.pBtn.setText('->')
-        else:
-            self.pBtn.setText('||')
-            
-    def onCli(self,e):       
-        try:
-            xS1 = int(self.l1.text())
-            yS1 = int(self.l2.text())
-            xL1 = int(self.l3.text())
-            yL1 = int(self.l4.text())
-            rr = int(self.l5.text())
-        except:
-            print ('set error')
-            return
-        if xS1<0 or yS1<0 or yL1<=0 or xL1<=0 or  xS1+xL1>xLen or yS1+yL1>yLen:
-            print ('set error')
-            return            
-        
-        if rr<0:
-            self.reso = 0
-        else:
-            self.reso = (rr//3)*3
-            
-        self.posCache = (xS1,yS1,xL1,yL1)        
+        self.xStart , self.yStart , self.xLen , self.yLen = g_para
+        self.ini()
+        self.setGeometry(QRect(200,200,800,500))        
+        self.th = t = threading.Thread(target=thr)
+        t.setDaemon(True)
+       # t.start()
+        self.q = q = qThr()
+        q.start()
         
     def ini(self):
         v = QVBoxLayout()
@@ -133,11 +140,11 @@ class ww(QWidget):
         self.pBtn = q2 = QPushButton('||')
         q2.clicked.connect(self.onP)
         
-        self.l1 = myLine('1000')
-        self.l2 = myLine('800')
-        self.l3 = myLine('50')
-        self.l4 = myLine('50')
-        self.l5 = myLine('0')
+        self.l1 = myLine(str(self.xStart))
+        self.l2 = myLine(str(self.yStart))
+        self.l3 = myLine(str(self.xLen))
+        self.l4 = myLine(str(self.yLen))
+        self.l5 = myLine(str(self.reso))
 
         h.addWidget(self.l1)
         h.addWidget(self.l2)
@@ -150,6 +157,29 @@ class ww(QWidget):
         v.addLayout(h)
         v.addStretch()
         self.setLayout(v)
+        
+    def onTimer(self):
+        self.repaint()  
+        
+    def onCli(self,e):       
+        try:
+            xS1 = int(self.l1.text())
+            yS1 = int(self.l2.text())
+            xL1 = int(self.l3.text())
+            yL1 = int(self.l4.text())
+            rr = int(self.l5.text())
+        except:
+            print ('set error')
+            return
+        if xS1<0 or yS1<0 or yL1<=0 or xL1<=0 or  xS1+xL1>xCapLen or yS1+yL1>yCapLen:
+            print ('set error')
+            return            
+        
+        if rr<0:
+            self.reso = 0
+        else:
+            self.reso = (rr//3)*3
+        self.xStart,self.yStart,self.xLen,self.yLen = xS1,yS1,xL1,yL1      
         
     def eventFilter(self,obj,e):
         if e.type() == QEvent.KeyPress:
@@ -195,23 +225,67 @@ class ww(QWidget):
         
         return super().eventFilter(obj,e)
     
-    def aa(self):
-        self.repaint()
-        
-    def dealRead(self):
-        self.data += self.tcpCli.readAll()
+    def onP(self,e):
+        self.pause = not self.pause
+        if self.pause:            
+            self.pBtn.setText('->')
+        else:
+            self.pBtn.setText('||')   
+    def drawOne(self,ty,v,i,j,im):
+        redCount = v/255*self.reso*self.reso/3
+        redCo = 0
+        if ty=='r':
+            cc = Qt.red
+            gap = 0
+        if ty=='g':
+            cc = Qt.green
+            gap = self.reso//3
+        if ty=='b':
+            cc = Qt.blue
+            gap = self.reso*2//3
+            
+        for ii in range(i*self.reso+gap,gap+i*self.reso+self.reso//3):
+            for jj in range(j*self.reso,(j+1)*self.reso):
+                if redCo >= redCount:
+                    im.setPixelColor(ii,jj,Qt.black)
+                    continue                
+                im.setPixelColor(ii,jj,cc)
+                redCo += 1  
                 
-    def dealClose(self):    
-        #print('tcpOver',len(self.data),time.monotonic()-self.tcpStartTime)
-        self.tcpCli.close()
-        self.tcpCli.deleteLater()
-        self.tcpCli = None
-        if not self.data:
-            return
-        d = self.data
-        self.data = b''        
-        im = QPixmap(QSize(xL,yL)).toImage()   
+    def draw(self,d,xl,yl):      
+        painter = QPainter(self)
+        pa = maxX
+        if xl*self.reso < maxX:
+            pa = xl*self.reso
+        pb = maxY
+        if yl*self.reso < maxY:
+            pb = yl*self.reso            
+        im = QPixmap(QSize(pa,pb)).toImage()
         
+        for i in range(xl):
+            for j in range(yl):
+                if (i+1)*self.reso>maxX or (j+1)*self.reso>maxY:
+                    continue
+                
+                val = i+j*xl
+                one = d[val*4:val*4+4]
+                argb = struct.unpack('I',one)[0]
+                b = (argb>>16)&0xFF;
+                g = (argb>>8)&0xFF;
+                r = (argb>>0)&0xFF;                 
+                self.drawOne('r',r,i,j,im)
+                self.drawOne('g',g,i,j,im)
+                self.drawOne('b',b,i,j,im)
+        painter.drawImage(QRect(drawP1,drawP2,im.width(),im.height()),im)                       
+            
+    def setPara(self):
+        global g_para
+        g_lock.acquire()
+        g_para = (self.xStart,self.yStart,self.xLen,self.yLen)
+        g_lock.release()    
+    
+    def drawOri(self,d,xL,yL):
+        im = QPixmap(QSize(xL,yL)).toImage()           
         for j in range(yL):
             for i in range(xL):
                 val = i+j*xL
@@ -220,29 +294,20 @@ class ww(QWidget):
                 b = (argb>>16)&0xFF;
                 g = (argb>>8)&0xFF;
                 r = (argb>>0)&0xFF; 
-                im.setPixelColor(i,j,QColor(r,g,b))                              
-        self.pic = im   
-        self.repaint()        
-        
-    def dealConn(self):    
-        self.tcpCli.readyRead.connect(self.dealRead)
-        self.tcpCli.disconnected.connect(self.dealClose)        
-        dd = struct.pack('i',xS)+struct.pack('i',yS)+struct.pack('i',xL)+struct.pack('i',yL)        
-        self.tcpCli.write(dd)
-        
+                im.setPixelColor(i,j,QColor(r,g,b))                     
+        painter = QPainter(self)        
+        painter.drawImage(QRect(drawP1,drawP2,im.width(),im.height()),im) 
+    
     def paintEvent(self, e):     
         #return super().paintEvent(e)
-        global xS,yS,xL,yL
-        self.draw()                
-        if self.tcpCli or self.pause:                          
-            return        
-        if self.posCache:                     
-            xS,yS,xL,yL = self.posCache
-            self.posCache = None            
-        self.tcpStartTime = time.monotonic()            
-        self.tcpCli = s = QTcpSocket(self)
-        s.connected.connect(self.dealConn)
-        s.connectToHost('10.23.185.230',8899)
+        self.setPara()  
+        g_lock.acquire()        
+        da,xLenB,yLenB = g_buf[0],g_buf[1],g_buf[2]
+        g_lock.release()
+        if self.reso == 0:
+            return self.drawOri(da,xLenB,yLenB)                
+        self.draw(da,xLenB,yLenB)                
+
 
 a = QApplication([])        
 window = w = ww()
