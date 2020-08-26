04fcaa3f0377e3d64c245d1c0e385d690b80fcce
zhengyitian <37394193+zhengyitian@users.noreply.github.com>
2020-08-26 23:19:38 +0800

    Update server.h

--- server.h
+++ server.h
@@ -16,7 +16,6 @@
 #include "qcursor.h"
 #include "QDesktopWidget"
 #include "QSettings"
-
 #include "QtNetwork/qtcpsocket.h"
 #include "QHBoxLayout"
 #include "qpushbutton.h"
@@ -24,15 +23,13 @@
 
 #include "qtcpserver.h"
 
-namespace Ui {
-class MainWindow;
-}
 
 
 class MainWindow : public QWidget
 {
     Q_OBJECT
 public:
+
     QTcpServer* server = new QTcpServer(this);
     QTcpSocket *socket;
     QByteArray data;
@@ -44,31 +41,24 @@ public:
         connect(server, SIGNAL(newConnection()), SLOT(newConnection()));
     }
 
-
-
-    ~MainWindow(){}
-
-
-
 public slots:
     void newConnection()
     {
         socket = server->nextPendingConnection();
-        connect(socket, SIGNAL(readyRead()), SLOT(dealRead()));
-    }
+        socket->waitForReadyRead();
 
-
-
-
-    void dealRead()
-    {
         auto a = socket->readAll();
+        if(a.size()!=16)
+        {
+            socket->deleteLater();
+            socket = nullptr;
+            return;
+        }
         int xs,ys,xl,yl;
         memcpy((char*)&xs,a.data(),4);
         memcpy((char*)&ys,a.data()+4,4);
         memcpy((char*)&xl,a.data()+8,4);
         memcpy((char*)&yl,a.data()+12,4);
-
         QScreen *screen  =  QGuiApplication::primaryScreen();
         auto im=screen->grabWindow(0,xs, ys, xl, yl).toImage();
         QByteArray d;
@@ -81,20 +71,14 @@ public slots:
                 d.push_back(qGreen(argb));
                 d.push_back(qBlue(argb));
                 d.push_back(88);
-
             }
         }
-        qDebug()<<d.size();
         socket->write(d.data(),d.size());
-
-        socket->waitForBytesWritten();
+        socket->waitForBytesWritten(5000);
         socket->close();
         socket->deleteLater();
         socket = nullptr;
     }
 };
-
-
-
 
 #endif
